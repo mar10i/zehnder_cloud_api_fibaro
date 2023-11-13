85cb9b35f0361ef6f6fe68f1ac3b5aee10772394
Maurice van der Lee <mauricevanderlee@users.noreply.github.com>
2023-11-13 20:11:41 +0100

    Update Config.lua

--- dev/Config.lua
+++ dev/Config.lua
@@ -14,12 +14,20 @@ function Config:getBaseUrl()
     return self.baseUrl
 end
 
-function Config:getToken()
-    return Globals:get('zehnder_atoken')
-end
+--function Config:getToken()
+--    return Globals:get('zehnder_atoken')
+--end
 
 function Config:getSubscriptionKey()
     return self.subscriptionKey
+end
+
+function Config:getApiKeyName()
+    return self.apiKeyName
+end
+
+function Config:getApiKeySecret()
+    return self.apiKeySecret
 end
 
 function Config:getDeviceID()
@@ -37,6 +45,8 @@ end
 function Config:init()
   self.baseUrl = ''
   self.subscriptionKey = tostring(self.app:getVariable('SubscriptionKey'))
+  self.apiKeyName = tostring(self.app:getVariable('ApiKeyName'))
+  self.apiKeySecret = tostring(self.app:getVariable('ApiKeySecret'))
   self.deviceID = tostring(self.app:getVariable('DeviceID'))
   self.buildingId = tostring(self.app:getVariable('BuildingId'))
   self.interval = self.app:getVariable('Interval')
@@ -44,6 +54,14 @@ function Config:init()
   if not self.subscriptionKey or self.subscriptionKey == '' then
     self.app:setVariable('SubscriptionKey','')
     self.subscriptionKey = ''
+  end
+  if not self.apiKeyName or self.apiKeyName == '' then
+    self.app:setVariable('ApiKeyName','')
+    self.apiKeyName = ''
+  end
+  if not self.apiKeySecret or self.apiKeySecret == '' then
+    self.app:setVariable('ApiKeySecret','')
+    self.apiKeySecret = ''
   end
   if not self.deviceID or self.deviceID == '' then
     self.app:setVariable('DeviceID','')
